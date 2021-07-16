91519a0fc73b2242ce40895fa9b15c84f094bc16
Captain Ahab <captainahab337@gmail.com>
2021-07-16 15:20:27 -0600

    moved folder and files out of develop

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const path = require("path");
+const mongoose = require("mongoose");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
+app.use(express.static("public"));
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/Fitness-Tracker", {
+  useNewUrlParser: true,
+  useFindAndModify: false,
+  useUnifiedTopology: true,
+  useCreateIndex: true
+});
+app.use(require("./routes/apiRoutes"));
+app.use(require("./routes/htmlRoutes"));
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}!`);
+});
